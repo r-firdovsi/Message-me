25bc02644ca7493a151eb0d94e0fa3cf7abf7d44
Firdovsi Rustamov <r-firdovsi@hotmail.com>
2021-02-18 14:32:07 +0400

    Make user and message model and migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "tamerlan", password: "Parol258")
+User.create(username: "elcin", password: "Parol258")
+User.create(username: "farid", password: "Parol258")
+
+Message.create(body: "Bro", user: User.last)
+Message.create(body: "Oke", user: User.last)
