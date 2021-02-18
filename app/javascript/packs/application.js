94a18b366918d400c3ddfb49f571dc679e33c04f
Firdovsi Rustamov <r-firdovsi@hotmail.com>
2021-02-18 15:23:28 +0400

    Make authentication system and enable flash messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,9 @@ Turbolinks.start()
 ActiveStorage.start()
 
 $(document).on('turbolinks:load', function () {
-    $('.ui.dropdown')
-        .dropdown();
+    $('.ui.dropdown').dropdown();
+    $('.message .close').on('click', function () {
+        $(this).closest('.message').transition('fade')
+        ;
+    });
 })
